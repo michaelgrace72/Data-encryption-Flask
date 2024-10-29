a3afc62d75a5cf6b5b99eb07d77f9bb381b74055
Azarel Grahandito <skootydack@gmail.com>
2024-10-29 21:13:09 +0700

    refactor: styling config

--- server/config.py
+++ server/config.py
@@ -3,14 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(32)
-    DB_USER = os.getenv('DB_USER', 'alvn')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'alvn12345')
-    DB_NAME = os.getenv('DB_NAME', 'ki')
-    
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    SECRET_KEY = os.getenv("SECRET_KEY") or os.urandom(32)
+    DB_USER = os.getenv("DB_USER", "arel")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_NAME = os.getenv("DB_NAME", "keamanan_informasi")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
-    PERMANENT_SESSION_LIFETIME = 1800  
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
+    PERMANENT_SESSION_LIFETIME = 1800
+
